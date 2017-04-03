0e3fe9d6dd9dbc2fdf381d81e28d942041dbe4d4
Christophe Van de Poel <Christophe.vdp@gmail.com>
2017-04-03 17:03:19 +0200

    ehh, ok. wth githubapp

--- src/app/sevices/wp-connect.service.ts
+++ src/app/sevices/wp-connect.service.ts
@@ -9,7 +9,12 @@ import { environment } from '../../environments/environment';
 
 @Injectable()
 export class WpConnectService {
-  apiRoot: string;
+
+    apiRoot: string;
+
+  // these will hold the Observables. Returning cached results if they can. 
+  latestPosts: {[id: number]: Post} = {};
+  authors: {[id: number]: Observable<Author>} = {};
 
   constructor(public http: Http) { 
     this.apiRoot = config.devConfig.apiRoot
@@ -19,22 +24,34 @@ export class WpConnectService {
     }
   }
 
-  public getLatestPosts(): Observable<Post[]> {
+  // This method will be a bit different from the others for now. I see no reason yet to fetch posts seperately if we want a list.
+  public getLatestPosts(force: boolean = false): Observable<Post[]> {
     return this.http.get(`${this.apiRoot}posts`)
       .map((response: Response) => {
         return(<any>response.json().map((post) => {
           let fullPost = new Post(post);
-          // this seems messy. But handy. It keeps the controller clean, so we do not have to chain calls.
-          this.getAuthorById(post.author).subscribe((author: Author) => fullPost.setPostAuthor(author))
+          
+          // the following line is messy. But handy. It keeps the controller-side clean, so we do not have to chain calls.
+          // we are, however mutating data after it's been sent (because getAuthorById is async). 
+          // Javascript lets us do this. Good indicator that we probably shouldn't.
+
+          this.getAuthorById(post.author, force).subscribe((author: Author) => fullPost.setPostAuthor(author));
+          this.latestPosts[fullPost.id] = fullPost;
           return fullPost;
         }));
       });
   }
 
-  public getAuthorById(id: number): Observable<Author> {
-    return this.http.get(`${this.apiRoot}users/${id}`)
-      .map((author: Response) => {
-          return new Author(author.json());
-      });
+  public getAuthorById(id: number, force: boolean = false): Observable<Author> {
+    if(!this.authors[id] || force){
+      this.authors[id] = this.http.get(`${this.apiRoot}users/${id}`)
+        .map((author: Response) => {
+            let fullAuthor = new Author(author.json());
+            return fullAuthor;
+        })
+        .publishReplay(1)
+        .refCount();
+    }
+    return this.authors[id];
   }
 }
