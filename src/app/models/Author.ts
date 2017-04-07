c8dfbc397ec776795e18698aa6593a39e0cb1e14
Christophe Van de Poel <Christophe.vdp@gmail.com>
2017-04-07 23:48:56 +0200

    Added avatar

--- src/app/models/Author.ts
+++ src/app/models/Author.ts
@@ -1,10 +1,12 @@
 export class Author {
     id: number;
     name: string;  
+    avatar: string;
 
     constructor(obj?: any
     ) {
         this.id =    obj && obj.id || null;
         this.name =  obj && obj.name || null;
+        this.avatar = obj && obj.avatar_urls && obj.avatar_urls[96] || null;
     }
 }
