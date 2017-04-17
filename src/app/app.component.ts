b8ef1fc414aef05aed8559bfe210713fcda22cba
Christophe Van de Poel <Christophe.vdp@gmail.com>
2017-04-17 15:28:09 +0200

    Some visual niceties & router handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Router, NavigationEnd } from '@angular/router';
+
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/publishReplay';
 import 'rxjs/add/operator/switchMap';
@@ -11,8 +13,15 @@ import 'rxjs/add/operator/switchMap';
 })
 export class AppComponent implements OnInit {
 
-  // inject wordpress connection service
-  constructor(){
+  constructor(private router: Router){
+    this.alwaysScrollToTopEventListener();
+  }
+
+  // very very basic scrolltop for routechanges. Doesn't handle 'back' button yet.
+  alwaysScrollToTopEventListener() {
+    this.router.events.subscribe((event) => {
+      (event instanceof NavigationEnd) ? window.scrollTo(0, 0) : null;
+    });
   }
 
   ngOnInit() {
--- src/app/utils/syntax.ts
+++ src/app/utils/syntax.ts
@@ -2,13 +2,15 @@ import SyntaxHighlighter, {registerBrush} from '../js/syntaxhighlighter/src/core
 import JsBrush from '../js/brush-javascript/brush';
 import { Brush as PhpBrush } from '../js/brush-php/brush';
 
+registerBrush(JsBrush);
+registerBrush(PhpBrush);
+
 function getSyntaxformatted(content: string): string {
-    registerBrush(JsBrush);
-    registerBrush(PhpBrush);
     let div = document.createElement("div");
     div.className = "post";
     div.innerHTML = content;
     let preElements: number = div.getElementsByTagName('pre').length;
+    
     let i: number = 0;
 
     for(i; i < preElements; i += 1) {
